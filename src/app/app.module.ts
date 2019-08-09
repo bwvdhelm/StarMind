07ba6fded53f3487dd41f09b731ca2436b3ec406
Bwvdhelm <bvanderh@gmail.com>
2019-08-09 18:22:26 +0200

    feat(mealplanner): Implemented basic mealplanner overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
+import { MatExpansionModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClickOutsideModule } from 'ng-click-outside';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FoodComponent } from './food/food.component';
+import { MealplannerComponent } from './food/mealplanner/mealplanner.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DotsAndSquaresComponent } from './projects/dots-and-squares/dots-and-squares.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { FoodComponent } from './food/food.component';
 
 @NgModule({
   declarations: [
@@ -19,13 +23,17 @@ import { FoodComponent } from './food/food.component';
     DotsAndSquaresComponent,
     ProjectsComponent,
     PageNotFoundComponent,
-    FoodComponent
+    FoodComponent,
+    MealplannerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
-    ClickOutsideModule
+    ClickOutsideModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
